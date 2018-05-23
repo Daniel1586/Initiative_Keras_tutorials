4c78312b637d5962ba6edd970b1cd5d84a9b22aa
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-05-23 22:27:04 +0800

    修正008_addition_rnn 序列到序列学习加法代码格式和注释

--- keras_working_with_text/008_addition_rnn.py
+++ keras_working_with_text/008_addition_rnn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
+# 序列到序列学习加法运算
 """
 An implementation of sequence to sequence learning for performing addition
 Input: "535+61"
@@ -23,31 +23,30 @@ Four digits reversed:
 Five digits reversed:
 + One layer LSTM (128 HN), 550k training examples = 99% train/test accuracy in 30 epochs
 """
+# Output after 5 epochs on CPU(i5-7500): ~0.7938
 
-from __future__ import print_function
-from keras.models import Sequential
-from keras import layers
 import numpy as np
+from keras import layers
 from six.moves import range
+from keras.models import Sequential
 
 
 class CharacterTable(object):
-    """Given a set of characters:
+    """
+    Given a set of characters:
     + Encode them to a one hot integer representation
     + Decode the one hot integer representation to their character output
     + Decode a vector of probabilities to their character output
     """
+    # 初始化字符表
     def __init__(self, chars):
-        """Initialize character table.
-        # Arguments
-            chars: Characters that can appear in the input.
-        """
         self.chars = sorted(set(chars))
         self.char_indices = dict((c, i) for i, c in enumerate(self.chars))
         self.indices_char = dict((i, c) for i, c in enumerate(self.chars))
 
     def encode(self, C, num_rows):
-        """One hot encode given string C.
+        """
+        One hot encode given string C.
         # Arguments
             num_rows: Number of rows in the returned one hot encoding. This is
                 used to keep the # of rows for each data the same.
@@ -63,28 +62,38 @@ class CharacterTable(object):
         return ''.join(self.indices_char[x] for x in x)
 
 
-class colors:
+class Colors:
     ok = '\033[92m'
     fail = '\033[91m'
     close = '\033[0m'
 
+
 # Parameters for the model and dataset.
-TRAINING_SIZE = 50000
 DIGITS = 3
 REVERSE = True
+TRAINING_SIZE = 50000
 
-# Maximum length of input is 'int + int' (e.g., '345+678'). Maximum length of
-# int is DIGITS.
+# Maximum length of input is 'int + int' (e.g., '345+678').
+# Maximum length of int is DIGITS.
 MAXLEN = DIGITS + 1 + DIGITS
 
 # All the numbers, plus sign and space for padding.
+# chars通过转换形成2个字典序列,1个list
+# chars: [' ','+','0','1','2','3','4','5','6','7','8','9']
+# char to indices:
+# {' ': 0, '+': 1, '0': 2, '1': 3, '2': 4, '3': 5, '4': 6, '5': 7, '6': 8, '7': 9, '8': 10, '9': 11}
+# indices to char:
+# {0: ' ', 1: '+', 2: '0', 3: '1', 4: '2', 5: '3', 6: '4', 7: '5', 8: '6', 9: '7', 10: '8', 11: '9'}
 chars = '0123456789+ '
 ctable = CharacterTable(chars)
 
 questions = []
 expected = []
 seen = set()
-print('Generating data...')
+
+# 生成测试用例
+# while循环将生成加法问题及答案,分别保存在questions和expected中
+print('========== 1.Generating data...')
 while len(questions) < TRAINING_SIZE:
     f = lambda: int(''.join(np.random.choice(list('0123456789'))
                     for i in range(np.random.randint(1, DIGITS + 1))))
@@ -102,38 +111,40 @@ while len(questions) < TRAINING_SIZE:
     # Answers can be of maximum size DIGITS + 1.
     ans += ' ' * (DIGITS + 1 - len(ans))
     if REVERSE:
-        # Reverse the query, e.g., '12+345  ' becomes '  543+21'. (Note the
-        # space used for padding.)
+        # Reverse the query, e.g., '12+345  ' becomes '  543+21'.
+        # (Note the space used for padding.)
         query = query[::-1]
     questions.append(query)
     expected.append(ans)
-print('Total addition questions:', len(questions))
+print('----- Total addition questions:', len(questions))
 
-print('Vectorization...')
-x = np.zeros((len(questions), MAXLEN, len(chars)), dtype=np.bool)
-y = np.zeros((len(questions), DIGITS + 1, len(chars)), dtype=np.bool)
+# 创建x,y,保存问题和答案,x:50000个问题,7位字符串,每一位对应12个字符
+print('========== 2.Vectorization...')
+x = np.zeros((len(questions), MAXLEN, len(chars)), dtype=np.bool)       # shape(50000,7,12)
+y = np.zeros((len(questions), DIGITS + 1, len(chars)), dtype=np.bool)   # shape(50000,7,12)
 for i, sentence in enumerate(questions):
     x[i] = ctable.encode(sentence, MAXLEN)
 for i, sentence in enumerate(expected):
     y[i] = ctable.encode(sentence, DIGITS + 1)
 
-# Shuffle (x, y) in unison as the later parts of x will almost all be larger
-# digits.
+# Shuffle (x, y) in unison as the later parts of x will almost all be larger digits.
+# 打散x,y的元素顺序
 indices = np.arange(len(y))
 np.random.shuffle(indices)
 x = x[indices]
 y = y[indices]
 
 # Explicitly set apart 10% for validation data that we never train over.
+# 切分训练集和测试集
 split_at = len(x) - len(x) // 10
 (x_train, x_val) = x[:split_at], x[split_at:]
 (y_train, y_val) = y[:split_at], y[split_at:]
 
-print('Training Data:')
+print('----- Training Data:')
 print(x_train.shape)
 print(y_train.shape)
 
-print('Validation Data:')
+print('----- Validation Data:')
 print(x_val.shape)
 print(y_val.shape)
 
@@ -143,45 +154,37 @@ HIDDEN_SIZE = 128
 BATCH_SIZE = 128
 LAYERS = 1
 
-print('Build model...')
+# 搭建神经网络模型
+print('========== 3.Building model...')
 model = Sequential()
 # "Encode" the input sequence using an RNN, producing an output of HIDDEN_SIZE.
-# Note: In a situation where your input sequences have a variable length,
-# use input_shape=(None, num_feature).
-model.add(RNN(HIDDEN_SIZE, input_shape=(MAXLEN, len(chars))))
-# As the decoder RNN's input, repeatedly provide with the last hidden state of
-# RNN for each time step. Repeat 'DIGITS + 1' times as that's the maximum
-# length of output, e.g., when DIGITS=3, max output is 999+999=1998.
-model.add(layers.RepeatVector(DIGITS + 1))
+# Note: In a situation where your input sequences have a variable length, use input_shape=(None, num_feature).
+model.add(RNN(HIDDEN_SIZE, input_shape=(MAXLEN, len(chars))))   # 输出(*,128)
+# As the decoder RNN's input, repeatedly provide with the last hidden state of RNN for each time step.
+# Repeat 'DIGITS + 1' times as that's the maximum length of output, e.g., when DIGITS=3, max output is 999+999=1998.
+model.add(layers.RepeatVector(DIGITS + 1))  # 输出(*,4,128)
 # The decoder RNN could be multiple layers stacked or a single layer.
 for _ in range(LAYERS):
-    # By setting return_sequences to True, return not only the last output but
-    # all the outputs so far in the form of (num_samples, timesteps,
-    # output_dim). This is necessary as TimeDistributed in the below expects
-    # the first dimension to be the timesteps.
-    model.add(RNN(HIDDEN_SIZE, return_sequences=True))
+    # By setting return_sequences to True, return not only the last output but all the outputs
+    # so far in the form of (num_samples, timesteps, output_dim). This is necessary as
+    # TimeDistributed in the below expects the first dimension to be the timesteps.
+    model.add(RNN(HIDDEN_SIZE, return_sequences=True))  # 输出(*,4,128)
 
 # Apply a dense layer to the every temporal slice of an input. For each of step
 # of the output sequence, decide which character should be chosen.
-model.add(layers.TimeDistributed(layers.Dense(len(chars))))
+model.add(layers.TimeDistributed(layers.Dense(len(chars))))     # 输出(*,4,12)
 model.add(layers.Activation('softmax'))
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-# Train the model each generation and show predictions against the validation
-# dataset.
+# Train the model each generation and show predictions against the validation dataset.
 for iteration in range(1, 200):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(x_train, y_train,
-              batch_size=BATCH_SIZE,
-              epochs=1,
-              validation_data=(x_val, y_val))
-    # Select 10 samples from the validation set at random so we can visualize
-    # errors.
+    model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=1, validation_data=(x_val, y_val))
+
+    # Select 10 samples from the validation set at random so we can visualize errors.
     for i in range(10):
         ind = np.random.randint(0, len(x_val))
         rowx, rowy = x_val[np.array([ind])], y_val[np.array([ind])]
@@ -192,7 +195,7 @@ for iteration in range(1, 200):
         print('Q', q[::-1] if REVERSE else q, end=' ')
         print('T', correct, end=' ')
         if correct == guess:
-            print(colors.ok + '☑' + colors.close, end=' ')
+            print(Colors.ok + '☑' + Colors.close, end=' ')
         else:
-            print(colors.fail + '☒' + colors.close, end=' ')
+            print(Colors.fail + '☒' + Colors.close, end=' ')
         print(guess)
